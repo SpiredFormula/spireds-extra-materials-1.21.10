21fa8340e8419bb0b54b7f9a4d2d50bba0394abd
Michael <96860840+SpiredFormula@users.noreply.github.com>
2025-12-17 20:05:35 +1100

    Add custom mithril ore worldgen and move datagen files

--- src/main/java/spired/spiredsextramaterials/SpiredsExtraMaterials.java
+++ src/main/java/spired/spiredsextramaterials/SpiredsExtraMaterials.java
@@ -2,9 +2,16 @@ package spired.spiredsextramaterials;
 
 import net.fabricmc.api.ModInitializer;
 
+
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 public class SpiredsExtraMaterials implements ModInitializer {
 	public static final String MOD_ID = "spireds-extra-materials";
 
@@ -12,15 +19,18 @@ public class SpiredsExtraMaterials implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+	public static final ResourceKey<PlacedFeature> CUSTOM_ORE_PLACED_KEY = ResourceKey.create(Registries.PLACED_FEATURE, ResourceLocation.fromNamespaceAndPath(SpiredsExtraMaterials.MOD_ID, "ore_custom"));
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
+
 		LOGGER.info("Hello Fabric world!");
 		ModBlocks.initialize();
 		ModItems.initialize();
+
+		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Decoration.UNDERGROUND_ORES, CUSTOM_ORE_PLACED_KEY);
 	}
 }
